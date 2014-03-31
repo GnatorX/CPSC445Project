6f4773cd178ed4cb41e81b6b6adc2a3e44d73a5a
Mint <mint@mint-VirtualBox.(none)>
2014-03-31 20:08:25 +0200

    modified:   CPSC 445 Project.py

--- CPSC 445 Project.py
+++ CPSC 445 Project.py
@@ -7,33 +7,61 @@ compareFile=sys.argv[2]
 sequenceFileData=open(sequenceFile,'r')
 compareFileData=open(compareFile,'r')
 
-
-def getSequence(fileData):	
+sequenceFileData=sequenceFileData.readlines()
+compareFileData=compareFileData.readlines()
+def getSequence(fileData,i):
 	sequence = []
 	structure= []
-	s = fileData.readline()
+	s = fileData[i]
 	if s.startswith('>'):
-		s=fileData.readline()
+		i=i+1
+		s=fileData[i]
 	s = s.rstrip()
 	for item in s:
 		sequence.append(item)
-	s = fileData.readline()
+	i=i+1
+	s = fileData[i]
 	s=s.rstrip()
 	for item in s:
 		structure.append(item)
 	return sequence,structure
 
+#def getKnownSequence(fileData):
+	
+
 unknownSequence=[]
 unknownStructure=[]
-unknownSequence,unknownStructure=getSequence(sequenceFileData)
+unknownSequence,unknownStructure=getSequence(sequenceFileData,0)
 
 knownSequences=[]
 knownStructures=[]
-
+#print unknownSequence,unknownStructure
 #print unknownSequence
-for num in range(0,len(compareFileData)):
-	kSe,kSt=getSequence(compareFileData)
-	knownSequences.append(kSe)
-	knownStructures.append(kSt)
-print knownSequences
+for i in range(0,len(compareFileData)-2):
+	sequence = []
+	structure= []
+	s = compareFileData[i]
+	if s.startswith('>'):
+		i=i+1
+		s=compareFileData[i]
+
+	s = s.rstrip()
+	for item in s:
+		sequence.append(item)
+	i=i+1
+	s = compareFileData[i]
+	s=s.rstrip()
+	for item in s:
+		structure.append(item)
+	knownSequences.append(sequence)
+	knownStructures.append(structure)
+#print knownSequences[0],knownStructures[0]
+
+########################################################################################################################
+#Obtain stem loops
+
+
+
+
+
 
