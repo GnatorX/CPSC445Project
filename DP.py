705bedf062e6e83786016f2645388a8d6637c198
jwchoi0722 <jwchoi0722@gmail.com>
2014-04-06 16:40:21 -0700

    pointer columns in progress

--- DP.py
+++ DP.py
@@ -7,17 +7,59 @@ alterScore=completeScore=1
 
 def getIndexingPairs(stemLoop):
 	indexingPairs=[]
+    # 4 pointers: current (x, y), (p(x), y), (x, s(y)), (p(x), s(y))
+    pointers = [[0,-1,-1,-1]]
+    # Parent node
+    parentNode = 0
+    # Current row number
+    i = 0
+    
 	for node in stemLoop:
+        # node - self (e.g. CG, CG)
 		indexingPairs.append(((node[0],node[1]),(node[0],node[1])))
+        i = i+1
+        pointers.append([i, -1, -1, prevNode])
+        currNode = i
+        # NOT terminal
 		if node[5] !=True:
-			for leaf in node[3]:
+			# left leaf - node (e.g. A-, CG)
+            leftLeafPred = currNode
+            for leaf in node[3]:
 				indexingPairs.append(((leaf,'-'),(node[0],node[1])))
-			for leaf in reversed(node[4]):
+                i = i+1
+                pointers.append([i, leftLeafPred, -1, -1])
+                leftLeafPred = i
+			# node - right leaf (e.g. CG, -U)
+            rightLeafSucc = currNode
+            for leaf in reversed(node[4]):
 				indexingPairs.append(((node[0],node[1]),('-',leaf)))
-			for leaf in node[3]:
+                i = i+1
+                pointers.append([i, -1, rightLeafPred, -1])
+                rightLeafSucc = i
+			# left leaf - right leaf (e.g. C-, -A)
+            # THIS PART IS MESSY, DON'T LOOK!!!
+            leftCounter = 0
+            rightCounter = 0
+            leafNodeStart = currNode + 1
+            nodeLeafStart = currNode + len(node[3]) + 1
+            currLeftLeafStart = nodeLeafStart
+            
+            predxy = nodeLeafStart
+            xsuccy = currNode + 1
+            predxsuccy = currNode
+            for leftLeaf in node[3]:
+                predxy = currLeftLeafStart
 				for rightLeaf in reversed(node[4]):
-					indexingPairs.append(((leaf,'-'),('-',rightLeaf)))
-		else:
+					indexingPairs.append(((leftLeaf,'-'),('-',rightLeaf)))
+                    i = i+1
+                    pointers.append([i, predxy, xsuccy, predxsuccy])
+                    predxy = predxy + 1
+                    xsuccy = i
+                currLeftLeafStart = currLeftLeafStart
+                leftCounter = leftCounter+1
+        # MESSY PART ENDS
+		# Terminal <--- COULD YOU LOOK INTO THIS PART?
+        else:
 			for leaf in node[3]:
 				indexingPairs.append(((leaf,'-'),(node[0],node[1])))
 			for leaf in reversed(node[3]):
@@ -25,7 +67,7 @@ def getIndexingPairs(stemLoop):
 			for num in range(0,len(node[3])):
 				for numK in reversed(range(num+1,len(node[3]))):
 					indexingPairs.append(((node[3][num],'-'),('-',node[3][numK])))
-	
+        prevNode = currNode
 	return indexingPairs
 			
 
