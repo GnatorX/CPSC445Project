087622718febf1e9cf71bd87cd9b092ee7f50ffc
jwchoi0722 <jwchoi0722@gmail.com>
2014-04-07 12:30:58 -0700

    recurrence done, please test

--- DP.py
+++ DP.py
@@ -2,11 +2,17 @@ import math
 import numpy
 numpy.set_printoptions(threshold=numpy.nan)
 
-deletionScore=insertScore=1
+# Scoring Model
+deletionScore=1
+insertScore=1
 relabelScore=0.25
-arcCreateScore=arcDestroyScore=0.5
-alterScore=completeScore=1
+arcCreateScore=1
+arcDestroyScore=1
+alterScore=1
+completeScore=1.5
 pairDeletionScore=1.5
+pairInsertionScore=1.5
+pairRelabelScore=0.4
 
 INTERNALNODE=0
 LEFTLEAF=1
@@ -134,10 +140,11 @@ def printOutIndexingPairs(indexingPairs,baseSequence):
                 print '-',
     print ')'
 
+# INITIALIZATION
 def initialization(firstIndexingPair,secondIndexingPair,firstPointer,secondPointer):
    # print len(firstIndexingPair),len(firstPointer)
     D=numpy.zeros((len(firstIndexingPair)+1,len(secondIndexingPair)+1))
-
+    # First Column
     for num in range(1,len(firstIndexingPair)+1 ):
          #  print firstIndexingPair[num-1][2]
         if firstIndexingPair[num-1][2]==INTERNALNODE:
@@ -149,6 +156,7 @@ def initialization(firstIndexingPair,secondIndexingPair,firstPointer,secondPoint
             D[num,0]=D[firstPointer[num][1],0]+deletionScore
         elif firstIndexingPair[num-1][2]==LEAFLEAF:
             D[num,0]=D[firstPointer[num][3],0]+2*deletionScore
+    # First Row
     for num in range(1,len(secondIndexingPair)+1):
         if secondIndexingPair[num-1][2]==INTERNALNODE:
             print num,secondPointer[num][3],D[0,secondPointer[num][3]]
@@ -162,6 +170,99 @@ def initialization(firstIndexingPair,secondIndexingPair,firstPointer,secondPoint
             D[0,num]=D[0,secondPointer[num][3]]+2*deletionScore
     return D
 
+# RECURRENCE
+def recurrence(D, pairs1, pairs2, pointers1, pointers2):
+    # Row-by-Row
+    for xy in range(1, len(pointers1)):
+        pointer1 = pointers1[xy]
+        # Column-by-Column
+        for uv in range(1, len(pointers2)):
+            pointer2 = pointers2[uv]
+            # CASE 1: Node and Node
+            if pairs1[xy-1][2] == INTERNALNODE and pairs2[uv-1][2] == INTERNALNODE:
+                # 3 possible events:
+                # relabeling, deletion, insertion
+                relabeling = D[pointer1[3], pointer2[3]] + pairRelabelScore
+                deletion = D[pointer1[3], pointer2[0]] + pairDeletionScore
+                insertion = D[pointer1[0], pointer2[3]] + pairInsertionScore
+                score = min(relabeling, deletion, insertion)
+            # CASE 2: Leaf and Leaf
+            elif pairs1[xy-1][2] != INTERNALNODE and pairs2[uv-1][2] != INTERNALNODE:
+                scores = []
+                # 6 possible events, some may be undefined:
+                # 2 deletions, 2 insertions, 2 relabelings
+                if pointer1[1] != -1: 
+                    deletionx = D[pointer1[1], pointer2[0]] + deletionScore
+                    scores.append(deletionx)
+                if pointer1[2] != -1:
+                    deletiony = D[pointer1[2], pointer2[0]] + deletionScore
+                    scores.append(deletiony)
+                if pointer2[1] != -1:
+                    insertionu = D[pointer1[0], pointer2[1]] + insertScore
+                    scores.append(insertionu)
+                if pointer2[2] != -1:
+                    insertionv = D[pointer1[0], pointer2[2]] + insertScore
+                    scores.append(insertionv)
+                if pointer1[1] != -1 and pointer2[1] != -1:
+                    relabelingxu = D[pointer1[1], pointer2[1]] + relabelScore
+                    scores.append(relabelingxu)
+                if pointer1[2] != -1 and pointer2[2] != -1:
+                    relabelingyv = D[pointer1[2], pointer2[2]] + relabelScore
+                    scores.append(relabelingyv)
+                score = min(scores)
+            # CASE 3: Node and Leaf
+            elif pairs1[xy-1][2] == INTERNALNODE and pairs2[uv-1][2] != INTERNALNODE:
+                scores = []
+                # 6 possible events, some may be undefined:
+                # deletion, 2 insertions, 2 alterings, ark-breaking
+                if pointer1[3] != -1:
+                    deletion = D[pointer1[3], pointer2[0]] + deletionScore
+                    scores.append(deletion)
+                    if pointer2[1] != -1:
+                        alteringu = D[pointer1[3], pointer2[1]] + alterScore
+                        scores.append(alteringu)
+                    if pointer2[2] != -1:
+                        alteringv = D[pointer1[3], pointer2[2]] + alterScore
+                        scores.append(alteringv)
+                    if pointer2[3] != -1:
+                        arcbreak = D[pointer1[3], pointer2[3]] + arcDestroyScore
+                        scores.append(arcbreak)
+                if pointer2[1] != -1:
+                    insertionu = D[pointer1[0], pointer2[1]] + insertScore
+                    scores.append(insertionu)
+                if pointer2[2] != -1:
+                    insertionv = D[pointer1[0], pointer2[2]] + insertScore
+                    scores.append(insertionv)
+                score = min(scores)
+            # CASE 4: Leaf and Node
+            elif pairs1[xy-1][2] != INTERNALNODE and pairs2[uv-1][2] == INTERNALNODE:
+                scores = []
+                # 6 possible events, some may be undefined:
+                # insertion, 2 deletions, 2 completion, arc-creation
+                if pointer2[3] != -1:
+                    insertion = D[pointer1[0], pointer2[3]] + insertScore
+                    scores.append(insertion)
+                    if pointer1[1] != -1:
+                        completex = D[pointer1[1], pointer2[3]] + completeScore
+                        scores.append(completex)
+                    if pointer1[2] != -1:
+                        completey = D[pointer1[2], pointer2[3]] + completeScore
+                        scores.append(completey)
+                    if pointer1[3] != -1:
+                        arccreate = D[pointer1[3], pointer2[3]] + arcCreateScore
+                        scores.append(arccreate)
+                if pointer1[1] != -1:
+                    deletionx = D[pointer1[1], pointer2[0]] + deletionScore
+                    scores.append(deletionx)
+                if pointer1[2] != -1:
+                    deletiony = D[pointer1[2], pointer2[0]] + deletionScore
+                    scores.append(deletiony)
+                score = min(scores)
+            D[xy, uv] = score
+    return D
+
+
+
 def findMin(inputStemLoop,outputStemLoop,inputSequence,outputSequence):
    
     inputIndexingPair,inPointers=getIndexingPairs(inputStemLoop)
@@ -170,7 +271,10 @@ def findMin(inputStemLoop,outputStemLoop,inputSequence,outputSequence):
     outputIndexingPair,outPointers=getIndexingPairs(outputStemLoop)
   #  print outputIndexingPair[0][2]
     D= initialization(inputIndexingPair,outputIndexingPair,inPointers,outPointers)
-    print D
+    #print D
+    D2 = recurrence(D, resultPairs1,resultPairs2,resultPointers1,resultPointers2)
+    print 'DISTANCE MATRIX : '
+    print D2
     
     
 
