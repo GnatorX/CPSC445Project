6713edf2301deb7dd13212c7e566e9f3e552872e
jwchoi0722 <jwchoi0722@gmail.com>
2014-04-07 22:28:37 -0700

    traceback work-in-progress, please test

--- DP.py
+++ DP.py
@@ -171,10 +171,21 @@ def initialization(firstIndexingPair,secondIndexingPair,firstPointer,secondPoint
     return D
 
 # RECURRENCE
+# D: Distance Matrix
+# pairs1: list of indexing pairs for the first stem loop (rows)
+# pairs2: list of indexing pairs for the second stem loop (columns)
+# pointers1: list of tuples for the first stem loop, each with 4 pointers to other rows
+# pointers2: list of tuples for the second stem loop, each with 4 pointers to other columns
+# Returns: updated D and P
 def recurrence(D, pairs1, pairs2, pointers1, pointers2):
+    # P: Pointer Matrix for the event that resulted in the minimum score
+    # list of 2-tuples containing two integers from {0,1,2,3},
+    # where 0 = (x, y); 1 = (p(x), y); 2 = (x, s(y)); 3 = (p(x), s(y))
+    P = []
     # Row-by-Row
     for xy in range(1, len(pointers1)):
         pointer1 = pointers1[xy]
+        Prow = []
         # Column-by-Column
         for uv in range(1, len(pointers2)):
             pointer2 = pointers2[uv]
@@ -185,7 +196,8 @@ def recurrence(D, pairs1, pairs2, pointers1, pointers2):
                 relabeling = D[pointer1[3], pointer2[3]] + pairRelabelScore
                 deletion = D[pointer1[3], pointer2[0]] + pairDeletionScore
                 insertion = D[pointer1[0], pointer2[3]] + pairInsertionScore
-                score = min(relabeling, deletion, insertion)
+                scores = [(relabeling, 3, 3), (deletion, 3, 0), (insertion, 0, 3)]
+                score = min(scores, key = lambda tup: tup[0])
             # CASE 2: Leaf and Leaf
             elif pairs1[xy-1][2] != INTERNALNODE and pairs2[uv-1][2] != INTERNALNODE:
                 scores = []
@@ -193,23 +205,23 @@ def recurrence(D, pairs1, pairs2, pointers1, pointers2):
                 # 2 deletions, 2 insertions, 2 relabelings
                 if pointer1[1] != -1: 
                     deletionx = D[pointer1[1], pointer2[0]] + deletionScore
-                    scores.append(deletionx)
+                    scores.append((deletionx, 1, 0))
                 if pointer1[2] != -1:
                     deletiony = D[pointer1[2], pointer2[0]] + deletionScore
-                    scores.append(deletiony)
+                    scores.append((deletiony, 2, 0))
                 if pointer2[1] != -1:
                     insertionu = D[pointer1[0], pointer2[1]] + insertScore
-                    scores.append(insertionu)
+                    scores.append((insertionu, 0, 1))
                 if pointer2[2] != -1:
                     insertionv = D[pointer1[0], pointer2[2]] + insertScore
-                    scores.append(insertionv)
+                    scores.append((insertionv, 0, 2))
                 if pointer1[1] != -1 and pointer2[1] != -1:
                     relabelingxu = D[pointer1[1], pointer2[1]] + relabelScore
-                    scores.append(relabelingxu)
+                    scores.append((relabelingxu, 1, 1))
                 if pointer1[2] != -1 and pointer2[2] != -1:
                     relabelingyv = D[pointer1[2], pointer2[2]] + relabelScore
-                    scores.append(relabelingyv)
-                score = min(scores)
+                    scores.append((relabelingyv, 2, 2))
+                score = min(scores, key = lambda tup: tup[0])
             # CASE 3: Node and Leaf
             elif pairs1[xy-1][2] == INTERNALNODE and pairs2[uv-1][2] != INTERNALNODE:
                 scores = []
@@ -217,23 +229,23 @@ def recurrence(D, pairs1, pairs2, pointers1, pointers2):
                 # deletion, 2 insertions, 2 alterings, ark-breaking
                 if pointer1[3] != -1:
                     deletion = D[pointer1[3], pointer2[0]] + deletionScore
-                    scores.append(deletion)
+                    scores.append((deletion, 3, 0))
                     if pointer2[1] != -1:
                         alteringu = D[pointer1[3], pointer2[1]] + alterScore
-                        scores.append(alteringu)
+                        scores.append((alteringu, 3, 1))
                     if pointer2[2] != -1:
                         alteringv = D[pointer1[3], pointer2[2]] + alterScore
-                        scores.append(alteringv)
+                        scores.append((alteringv, 3, 2))
                     if pointer2[3] != -1:
                         arcbreak = D[pointer1[3], pointer2[3]] + arcDestroyScore
-                        scores.append(arcbreak)
+                        scores.append((arcbreak, 3, 3))
                 if pointer2[1] != -1:
                     insertionu = D[pointer1[0], pointer2[1]] + insertScore
-                    scores.append(insertionu)
+                    scores.append((insertionu, 0, 1))
                 if pointer2[2] != -1:
                     insertionv = D[pointer1[0], pointer2[2]] + insertScore
-                    scores.append(insertionv)
-                score = min(scores)
+                    scores.append((insertionv, 0, 2))
+                score = min(scores, key = lambda tup: tup[0])
             # CASE 4: Leaf and Node
             elif pairs1[xy-1][2] != INTERNALNODE and pairs2[uv-1][2] == INTERNALNODE:
                 scores = []
@@ -241,27 +253,86 @@ def recurrence(D, pairs1, pairs2, pointers1, pointers2):
                 # insertion, 2 deletions, 2 completion, arc-creation
                 if pointer2[3] != -1:
                     insertion = D[pointer1[0], pointer2[3]] + insertScore
-                    scores.append(insertion)
+                    scores.append((insertion, 0, 3))
                     if pointer1[1] != -1:
                         completex = D[pointer1[1], pointer2[3]] + completeScore
-                        scores.append(completex)
+                        scores.append((completex, 1, 3))
                     if pointer1[2] != -1:
                         completey = D[pointer1[2], pointer2[3]] + completeScore
-                        scores.append(completey)
+                        scores.append((completey, 2, 3))
                     if pointer1[3] != -1:
                         arccreate = D[pointer1[3], pointer2[3]] + arcCreateScore
-                        scores.append(arccreate)
+                        scores.append((arccreate, 3, 3))
                 if pointer1[1] != -1:
                     deletionx = D[pointer1[1], pointer2[0]] + deletionScore
-                    scores.append(deletionx)
+                    scores.append((deletionx, 1, 0))
                 if pointer1[2] != -1:
                     deletiony = D[pointer1[2], pointer2[0]] + deletionScore
-                    scores.append(deletiony)
-                score = min(scores)
-            D[xy, uv] = score
-    return D
+                    scores.append((deletiony, 2, 0))
+                score = min(scores, key = lambda tup: tup[0])
+            D[xy, uv] = score[0]
+            Prow.append((score[1], score[2]))
+        P.append(Prow)
+    return D,P
 
+# TRACEBACK
+def traceback(D, P, resultPairs1, resultPairs2, resultPointers1, resultPointers2):
 
+    # Extract 'terminal' indexing pairs by finding the lowest internal node
+    lastNode1 = 0
+    iter = 0
+    for pair in resultPairs1:
+        if pair[2] == INTERNALNODE: lastNode1 = iter+1
+        iter = iter+1
+    lastNode2 = 0
+    iter = 0
+    for pair in resultPairs2:
+        if pair[2] == INTERNALNODE: lastNode2 = iter+1
+        iter = iter+1
+    print 'lastNode1 = ' + str(lastNode1) + ', lastNode2 = ' + str(lastNode2)
+
+    terminalSubmatrix = D[lastNode1:, lastNode2:]
+    print 'Terminal indexing pairs dimensions: ' + str( terminalSubmatrix.shape )
+
+    # Find the minimum score and its coordinates
+    terminalMin = terminalSubmatrix.min()
+    print 'Minimum score between terminal indexing pairs: ' + str( terminalMin )
+    terminalMinIndex = numpy.unravel_index( terminalSubmatrix.argmin(), terminalSubmatrix.shape )
+    rowindex = lastNode1 + terminalMinIndex[0]
+    colindex = lastNode2 + terminalMinIndex[1]
+    terminalMinIndex = (rowindex, colindex)
+    print 'Coordinates for the minimum score: ' + str( terminalMinIndex )
+
+    # Backtrack
+    currrow = rowindex
+    currcol = colindex
+    backPointer = 0
+    # path: list of tuples containing 
+    # (1) an indexing pair from first sequence, {2) an indexing pair from second sequence, 
+    # (3) score
+    path = []
+    stop = 0
+    while stop == 0:
+        #print 'currrow=' + str(currrow) + ', currcol=' + str(currcol)
+        #firstPair = resultPairs1[currrow-1]
+        #secondPair = resultPairs2[currcol-1]
+        
+        path.append((resultPairs1[currrow-1], resultPairs2[currcol-1], D[currrow, currcol]))
+        
+        backPointers = P[currrow-1][currcol-1]
+        #print backPointers
+        backPointer1 = backPointers[0]
+        backPointer2 = backPointers[1]
+        
+        #print resultPointers1[currrow]
+        #print resultPointers2[currcol]
+        currrow = resultPointers1[currrow][backPointer1]
+        currcol = resultPointers2[currcol][backPointer2]
+        
+        if currrow <= 0 or currcol <= 0:
+            stop = 1
+
+    return path
 
 def findMin(inputStemLoop,outputStemLoop,inputSequence,outputSequence):
    
@@ -272,9 +343,12 @@ def findMin(inputStemLoop,outputStemLoop,inputSequence,outputSequence):
   #  print outputIndexingPair[0][2]
     D= initialization(outputIndexingPair,inputIndexingPair,outPointers,inPointers)
     #print D
-    D2 = recurrence(D, outputIndexingPair,inputIndexingPair,outPointers,inPointers)
-    print 'DISTANCE MATRIX : '
-    print D2
+    D2,P = recurrence(D, outputIndexingPair,inputIndexingPair,outPointers,inPointers)
+    #print 'DISTANCE MATRIX : '
+    #print D2
+    rawPath = traceback(D2, P, outputIndexingPair, inputIndexingPair, outPointers, inPointers)
+    print 'RAW TRACEBACK PATH :'
+    print rawPath
     
     
 
